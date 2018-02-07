e928218242699e14ec2c3c0b5ce080667fd86c1e
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-02-07 08:19:39 -0500

    Updated from Brython Server: 2/7/2018 8:19:38 AM

--- graphicsDemo.py
+++ graphicsDemo.py
@@ -6,17 +6,19 @@ from ggame import *
 
 red=Color(0xff0000, 1) #this is the color red
 black=Color(0x000000, 1) #this is the color black
-green=Color(0x00ff00, 1) 
-blue=Color(0x0000ff, 1)
+green=Color(0x00ff00, 1) #this is the color green
+blue=Color(0x0000ff, 1) #this is the color red
 
 blackOutline=LineStyle(1,black)
 
 redRectangle=RectangleAsset(200,100,blackOutline,red) #(width, height, outline, fill color)
 blueCircle=CircleAsset(75, blackOutline, blue) #(radius, outline, fill color)
 greenEllipse=EllipseAsset(100, 50, blackOutline, green) #(width, height, outline, fill color)
+blackLine=LineAsset(50, 160, blackOutline) #(x endpoint, y endpoint, outline)
 
 Sprite(redRectangle)
 Sprite(blueCircle,(100, 100)) #Sprite(spriteName, (x, y))
 Sprite(greenEllipse, (200, 400))
+Sprite(blackLine)
 
 App().run()
