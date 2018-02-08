353690baf16257055b713f9bad853dd778fc5adf
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-02-08 12:14:59 -0500

    Updated from Brython Server: 2/8/2018 12:14:58 PM

--- house.py
+++ house.py
@@ -1,1 +1,15 @@
+#James Roth
+#2/8/18
+#house.py - using the graphics engine
 
+from ggame import *
+
+blackOutline=LineStyle (0.5, black)
+
+blue=Color(0x0000ff, 1)
+
+blueSquare=RectangleAsset(300, 125, blackOutline, blue)
+
+Sprite(blueSquare, (200,200))
+
+App().run()
