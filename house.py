b2f57802e8a84eed288624bc6685ee437bef811e
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-02-09 08:26:27 -0500

    Updated from Brython Server: 2/9/2018 8:26:26 AM

--- house.py
+++ house.py
@@ -8,11 +8,15 @@ blackOutline=LineStyle (0.5, black)
 whiteOutline=LineStyle (2, white)
 
 blue=Color(0x8890D5, 1)
+blue2=Color(0x7790E5, 1)
 
 blueSquare=RectangleAsset(300, 125, blackOutline, blue)
 blueTriangle=PolygonAsset([(100, 300), (500, 300), (300, 225)], whiteOutline, blue)
+blueDoor=RectangleAsset(50, 150, blackOutline, blue2)
+
 
 Sprite(blueSquare, (200,200))
 Sprite(blueTriangle, (150,125))
+Sprite(blueDoor, (325, 125))
 
 App().run()
