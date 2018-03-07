70517842ae0176f25975eb565b665b2a9b2e68b2
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-03-07 08:33:40 -0500

    Updated from Brython Server: 3/7/2018 8:33:39 AM

--- yield.py
+++ yield.py
@@ -1,1 +1,12 @@
+#James Roth
+#3/7/18
+#yield.py - yield sign
 
+red=Color(0xff0000)
+redOutline=LineStyle(red, 1)
+
+redTriangle=PolygonAsset([(0,0), (300, 0), (150, 100)], redOutline, red)
+
+Sprite(redTriangle)
+
+App().run()
