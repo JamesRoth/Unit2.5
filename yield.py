e3bdb0c951ff59d2ad89206647ded3b829af53d9
JamesRoth <35492763+JamesRoth@users.noreply.github.com>
2018-03-07 08:33:55 -0500

    Updated from Brython Server: 3/7/2018 8:33:54 AM

--- yield.py
+++ yield.py
@@ -2,7 +2,7 @@
 #3/7/18
 #yield.py - yield sign
 
-red=Color(0xff0000)
+red=Color(0xff0000, 1)
 redOutline=LineStyle(red, 1)
 
 redTriangle=PolygonAsset([(0,0), (300, 0), (150, 100)], redOutline, red)
